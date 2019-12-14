d8df457d4940bec55f593bcb25a26717181d9a85
Amanda Rolon <abrolon87@gmail.com>
2019-12-14 15:56:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,17 @@
 # Write your code here.
 def line(katz_deli)
-  if line(katz_deli).length == 0
-    puts "There is nobody in line."
+  if line(katz_deli)
+    puts
 
+  else
+    puts "The line is currently empty"
   end
+end
+
+def take_a_number
+
+end
+
+def now_serving
+
 end
