3c83919bba57eb2db0931504bb7999f3593253eb
Amanda Rolon <abrolon87@gmail.com>
2019-12-14 16:10:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    other_deli = "The line is currently: "
+    other_deli = "The line is currently:"
     katz_deli.each.with_index(1) do |customer, n|
       other_deli << " #{n}. #{customer}"
     end
