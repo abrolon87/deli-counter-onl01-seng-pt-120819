85981f7f88d9ff55186300dbf8b6ab85986d6c5c
Amanda Rolon <abrolon87@gmail.com>
2019-12-14 16:12:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome, #{name}. You are #{deli.size} in line."  
+  puts "Welcome, #{name}. You are #{katz_deli.size} in line."  
 end
 
 def now_serving
