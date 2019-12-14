13138e3cc74ca55be6f1846024a28003c8c513af
Amanda Rolon <abrolon87@gmail.com>
2019-12-14 16:12:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.size == 0
     puts "The line is currently empty."
   else
     other_deli = "The line is currently:"
@@ -11,8 +11,9 @@ def line(katz_deli)
   end
 end
 
-def take_a_number
-
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are #{deli.size} in line."  
 end
 
 def now_serving
